3f12e4b478b69cf7d0be546e47bdb99644601f84
Michael McMasters <jmcmasters411@gmail.com>
2025-12-08 16:06:58 -0500

    Make the Bow aim further in a straight line

--- Assets/_stealthArcher/scripts/gameObjects/weapons/Bow.cs
+++ Assets/_stealthArcher/scripts/gameObjects/weapons/Bow.cs
@@ -8,7 +8,8 @@ using TouchPhase = UnityEngine.TouchPhase;
 namespace _stealthArcher.scripts.weaponPrefabs {
 public class Bow : IWeapon {
 
-    private Trackpad trackpad;
+    // private Trackpad joystick;
+    private Joystick joystick;
 
     [Header("Aiming")]
     [SerializeField] private LineRenderer aimerLineRendererPrefab;
@@ -27,12 +28,12 @@ public class Bow : IWeapon {
 
 
     void Start() {
-        this.trackpad = new Trackpad(SpecificTouch.Right, Callback);
+        this.joystick = new Joystick(SpecificTouch.Right, Callback);
         this.playerObj = GameObjects.Player;
     }
 
     public override void HandleInput() {
-        trackpad?.HandleInput();
+        joystick?.HandleInput();
     }
 
     private void Callback(JoystickEvent joystickEvent, JoystickData joystickData) {
@@ -62,23 +63,42 @@ public class Bow : IWeapon {
         aimerLineRendererInitialMaterial = aimerLineRendererObj.GetComponent<Renderer>().material;
     }
 
-    private void ContinueAim(GameObject playerObj, Vector3 joystickPosition) {
-        aimerLineRendererObj.GetComponent<Renderer>().material = aimerLineRendererInitialMaterial;
-        
-        float distance = Vector3.Distance(playerObj.transform.position, joystickPosition) * 0.5f;
-        Vector3 midPoint = VectorUtil.NewPointInTargetsDirection(playerObj.transform.position, joystickPosition, distance);
-        midPoint.y = playerObj.transform.position.y + 5;
-        
-        List<Vector3> bezierPath = BezierCurveGenerator.CreateBezierCurve(playerObj.transform.position, midPoint, joystickPosition);
-        
-        aimerLineRendererObj.positionCount = bezierPath.Count;
-        for (int i = 0; i < bezierPath.Count; i++) {
-            aimerLineRendererObj.SetPosition(i, bezierPath[i]);
-        }
-        
-        this.aimData = new AimData(bezierPath);
-    }
+    // private void ContinueAim(GameObject playerObj, Vector3 joystickPosition) {
+    //     aimerLineRendererObj.GetComponent<Renderer>().material = aimerLineRendererInitialMaterial;
+    //     
+    //     float distance = Vector3.Distance(playerObj.transform.position, joystickPosition) * 0.5f;
+    //     Vector3 midPoint = VectorUtil.NewPointInTargetsDirection(playerObj.transform.position, joystickPosition, distance);
+    //     midPoint.y = playerObj.transform.position.y + 5;
+    //     
+    //     List<Vector3> bezierPath = BezierCurveGenerator.CreateBezierCurve(playerObj.transform.position, midPoint, joystickPosition);
+    //     
+    //     aimerLineRendererObj.positionCount = bezierPath.Count;
+    //     for (int i = 0; i < bezierPath.Count; i++) {
+    //         aimerLineRendererObj.SetPosition(i, bezierPath[i]);
+    //     }
+    //     
+    //     this.aimData = new AimData(bezierPath);
+    // }
 
+    private void ContinueAim(GameObject playerObj, Vector3 aroundPlayerPosition) {
+        aimerLineRendererObj.GetComponent<Renderer>().material = aimerLineRendererInitialMaterial;
+
+        float aimerHeight = playerObj.transform.position.y + playerObj.transform.localScale.y;
+        
+        float distance = 100f;
+        Vector3 startPoint = playerObj.transform.position;
+        startPoint.y = aimerHeight;
+        aroundPlayerPosition.y = aimerHeight;
+        
+        Vector3 endPoint = VectorUtil.NewPointInTargetsDirection(startPoint, aroundPlayerPosition, distance);
+        
+        aimerLineRendererObj.positionCount = 2;
+        aimerLineRendererObj.SetPosition(0, startPoint);
+        aimerLineRendererObj.SetPosition(1, endPoint);
+        
+        this.aimData = new AimData(new List<Vector3>() {playerObj.transform.position, endPoint});
+    }
+    
     private void HandleInCancelRange() {
         // aimerLineRendererObj.positionCount = 0;
         aimerLineRendererObj.GetComponent<Renderer>().material = aimerLineRendererCancelMaterial;
