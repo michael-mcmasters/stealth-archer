a45e3e944a54eb2f7fd9ca973884cad479d47471
Michael McMasters <jmcmasters411@gmail.com>
2025-12-15 01:08:12 -0500

    Add a POC climbing mechanic

--- Assets/_stealthArcher/scripts/constants/Constants.cs
+++ Assets/_stealthArcher/scripts/constants/Constants.cs
@@ -2,6 +2,7 @@ namespace _stealthArcher.scripts.constants {
 public class Constants {
 
     public static readonly string GROUND = "Ground";
+    public static readonly string WALL = "Wall";
     public static readonly string ENEMY = "Enemy";
     public static readonly string HEAD = "Head";
 
---
+++ Assets/_stealthArcher/scripts/gameObjects/player/PlayerClimbHandler.cs
@@ -0,0 +1,44 @@
+﻿using _stealthArcher.scripts.constants;
+using _stealthArcher.scripts.input;
+using UnityEngine;
+
+namespace _stealthArcher.scripts.gameObjects.player {
+public class PlayerClimbHandler : MonoBehaviour {
+
+    private GameObject canJumpIndicatorObj;
+    private Collider colliderToClimb;
+
+    
+    void Start() {
+        canJumpIndicatorObj = Instantiate(GameObjects.Cursor);
+        
+        GetComponent<OnTapHandler>().OnTapCallback = () => {
+            Debug.Log("OnTap called in Player!");
+            if (canJumpIndicatorObj.activeSelf) {
+                Debug.Log("Jumping!");
+                
+                CharacterController cc = GetComponent<CharacterController>();
+                cc.Move(Vector3.up * 5f);
+            } else {
+                Debug.Log("Can't jump");
+            }
+        };
+    }
+
+    void Update() {
+        float radius = 1;
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider col in hits) {
+            if (col.name.Contains(Constants.WALL)) {
+                canJumpIndicatorObj.SetActive(true);
+                canJumpIndicatorObj.transform.position = new Vector3(transform.position.x, 3, transform.position.z);
+                colliderToClimb = col;
+                return;
+            }
+        }
+        canJumpIndicatorObj.SetActive(false);
+        return;
+    }
+
+}
+}
